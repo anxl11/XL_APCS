a88f1f05a3dfcd3b9e3748ebbe52e05e7b842b8c
XuanAn Le <xl1820352@025Labs-iMac.local>
2019-12-12 10:12:57 -0600

    these are the latest 2

--- 033_BasicNested/BasicNested.java
+++ 033_BasicNested/BasicNested.java
@@ -2,10 +2,29 @@ import java.util.Scanner;
 public class BasicNested{
     public static void main (String[]args){
         Scanner scan = new Scanner(System.in);
-        System.out.println("Please input your word");
+        System.out.println("Please input your letter");
         String inp1 = new String(scan.next());
-        System.out.println("Please input your number of row");
+        System.out.println("Please input your number of rows");
+        int row = scan.nextInt();
+        System.out.println("Please input your number of columns");
+        int column = scan.nextInt();
+        for (int i =0; i<= row;i++){
+            System.out.println();
+            if (inp1.length()==1){
+                if(row<=20&&column<=20){
+                    for(int ii =0; ii< column;ii++){
+                        System.out.print(inp1);
+                    }
 
-        System.out.println("Please input your number of row");
+                }
+                else{
+                    System.out.print("your number of rows and columns can not exceed 20");
+                    break;
+                }
+            }
+            else{
+                System.out.print("your input must be a single letter");
+            }
+        }
     }
 }
---
+++ 034_HelloWorldButAMethod/HelloWorldButAMethod.java
@@ -0,0 +1,10 @@
+public class HelloWorldButAMethod{
+    String word = "Hello World";
+    public static void main(String[]args){
+        hello("world");
+    }
+    public static void hello(String a){
+
+        System.out.println("hello "+a);
+    }
+}
